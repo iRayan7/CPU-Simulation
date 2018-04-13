f9da188840233038f52cfcfb3b857f539ce192b2
WadhahEssam <wadah-wleed@hotmail.com>
2018-04-13 23:37:22 +0300

    test

--- src/main.java
+++ src/main.java
@@ -6,7 +6,6 @@ public class main {
 	public static void main(String[] args) throws IOException {
 		OperatingSystem OS = new OperatingSystem () ; 
 		OS.start() ; 
-		System.out.print("hi");
 		//foo
 	}
 
